6f2473bc8cd2e4a337b55f78ad3a58c76dc15ebe
Brylkovskyi MD <brylkovsky@gmail.com>
2020-01-29 20:39:44 +0200

    last

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ client.connect((err, client) => {
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
+  const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
     console.log("App now running on port", port);
   });
 });
